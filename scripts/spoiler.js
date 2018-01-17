017ea3e25b81d46bd79b65bbf47718495dd89d9c
BaliBalo <balibalo@pause-geek.fr>
2018-01-17 04:56:32 +0000

    Test adding mentions

--- scripts/spoiler.js
+++ scripts/spoiler.js
@@ -5,6 +5,7 @@ const path = require('path');
 const Discord = require('discord.js');
 const GIFEncoder = require('gifencoder');
 const Canvas = require('canvas');
+const Image = Canvas.Image;
 const request = require('request-promise-native');
 const getStream = require('get-stream');
 
@@ -22,15 +23,22 @@ function spoilerGif(text) {
 	// 36 for the 'gif' size + 5 extra padding
 	let width = ctx.measureText(defaultText).width + 41;
 	let lines = [];
+	let icons = [];
 	text.forEach(src => {
 		src = src.split(' ');
 		let line = src.shift();
 		let size = ctx.measureText(line).width;
 		while (src.length) {
-			let elem = src.shift();
+			let icon;
+			let elem = src.shift().replace(/^«««=([^» ]+)»»»/, (r, url) => {
+				icon = url;
+				return ' ';
+			});
+			let left = ctx.measureText(line + ' ').width;
 			let iSize = ctx.measureText(line + ' ' + elem).width;
 			if (iSize > maxWidth) {
 				lines.push(line);
+				left = 0;
 				width = Math.max(width, size);
 				line = elem;
 				size = ctx.measureText(line).width;
@@ -38,67 +46,84 @@ function spoilerGif(text) {
 				line += ' ' + elem;
 				size = iSize;
 			}
+			if (icon) {
+				icons.push({
+					line: lines.length,
+					left: left,
+					url: icon
+				});
+			}
 		}
 		lines.push(line);
 		width = Math.max(width, size);
 	});
 	lines = lines.slice(0, 20);
 
-	let padding = 5;
-	let w = width + 2 * padding;
-	let h = lines.length * lineHeight + 2 * padding;
+	return Promise.all(icons.map(icon => request({
+		url: icon.url,
+		encoding: null
+	}))).then(images => {
+		let padding = 5;
+		let w = width + 2 * padding;
+		let h = lines.length * lineHeight + 2 * padding;
 
-	c.width = w;
-	c.height = h;
-	let encoder = new GIFEncoder(w, h);
+		c.width = w;
+		c.height = h;
+		let encoder = new GIFEncoder(w, h);
 
-	let stream = encoder.createReadStream();
+		let stream = encoder.createReadStream();
 
-	let cfrom = new Canvas(w, h);
-	let cto = new Canvas(w, h);
-	let from = cfrom.getContext('2d');
-	let to = cto.getContext('2d');
+		let cfrom = new Canvas(w, h);
+		let cto = new Canvas(w, h);
+		let from = cfrom.getContext('2d');
+		let to = cto.getContext('2d');
 
-	from.font = font;
-	to.font = font;
-	from.textBaseline = 'top';
-	to.textBaseline = 'top';
-	to.fillStyle = '#36393e';
-	to.fillRect(0, 0, w, h);
-	from.fillStyle = '#36393e';
-	from.fillRect(0, 0, w, h);
-	from.fillStyle = 'rgba(255, 255, 255, 0.5)';
-	to.fillStyle = 'rgba(255, 255, 255, 0.7)';
-	from.fillText(defaultText, padding, padding);
-	lines.forEach((line, i) => to.fillText(line, padding, padding + lineHeight * i));
+		from.font = font;
+		to.font = font;
+		from.textBaseline = 'top';
+		to.textBaseline = 'top';
+		to.fillStyle = '#36393e';
+		to.fillRect(0, 0, w, h);
+		from.fillStyle = '#36393e';
+		from.fillRect(0, 0, w, h);
+		from.fillStyle = 'rgba(255, 255, 255, 0.5)';
+		to.fillStyle = 'rgba(255, 255, 255, 0.7)';
+		from.fillText(defaultText, padding, padding);
+		lines.forEach((line, i) => to.fillText(line, padding, padding + lineHeight * i));
+		icons.forEach((icon, i) => {
+			let img = new Image();
+			img.src = images[i];
+			to.drawImage(img, icon.left, padding + lineHeight * icon.line, 15, 15);
+		});
 
-	encoder.start();
-	encoder.setRepeat(-1);
-	encoder.setDelay(20);
+		encoder.start();
+		encoder.setRepeat(-1);
+		encoder.setDelay(20);
 
-	let bandSize = 10;
-	let duration = 40;
-	let bandDuration = .4;
-	let easing = t => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
-	function frame(i) {
-		let p = i / (duration - 1);
-		for (let l = 0; l < w; l += bandSize) {
-			let bandFrom = (1 - bandDuration) * l / w;
-			let bandProgress = Math.min(Math.max((p - bandFrom) / bandDuration, 0), 1);
-			bandProgress = easing(bandProgress);
-			let y = -bandProgress * h;
-			ctx.drawImage(cfrom, l, 0, bandSize, h, l, y, bandSize, h);
-			ctx.drawImage(cto, l, 0, bandSize, h, l, y + h, bandSize, h);
+		let bandSize = 10;
+		let duration = 40;
+		let bandDuration = .4;
+		let easing = t => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+		function frame(i) {
+			let p = i / (duration - 1);
+			for (let l = 0; l < w; l += bandSize) {
+				let bandFrom = (1 - bandDuration) * l / w;
+				let bandProgress = Math.min(Math.max((p - bandFrom) / bandDuration, 0), 1);
+				bandProgress = easing(bandProgress);
+				let y = -bandProgress * h;
+				ctx.drawImage(cfrom, l, 0, bandSize, h, l, y, bandSize, h);
+				ctx.drawImage(cto, l, 0, bandSize, h, l, y + h, bandSize, h);
+			}
+			encoder.addFrame(ctx);
 		}
-		encoder.addFrame(ctx);
-	}
-	for (let i = 0; i < duration; i++) {
-		frame(i);
-	}
+		for (let i = 0; i < duration; i++) {
+			frame(i);
+		}
 
-	encoder.finish();
+		encoder.finish();
 
-	return stream;
+		return stream;
+	});
 }
 
 function uploadFile(content, id) {
@@ -132,9 +157,24 @@ function uploadFile(content, id) {
 module.exports = function(message, content) {
 	if (!content) return;
 	message.delete();
-	return uploadFile(content, message.id).then(pasteUrl => {
-		return message.reply(pasteUrl ? '(version texte: <'+pasteUrl+'>)' : '', {
-			files: [ new Discord.Attachment(spoilerGif(content), 'spoiler.gif') ]
+
+	// use mentions.USERS_PATTERN on next major (discord.js 12)
+	content = content
+		.replace(/<@!?(1|\d{17,19})>/g, (m, id) => {
+			let user = message.mentions.members.get(id);
+			let avatar = '';
+			if (user.avatarURL) {
+				avatar = '«««='+user.avatarURL+'»»»';
+			}
+			return avatar + '@' + user.username;
+		})
+		.replace(/<#(\d{17,19})>/g, (m, id) => '#' + message.mentions.channels.get(id).username);
+
+	return uploadFile(content.replace(/«««=([^» ]+)»»»/g, ''), message.id).then(pasteUrl => {
+		return spoilerGif(content).then(gif => {
+			return message.reply(pasteUrl ? '(version texte: <'+pasteUrl+'>)' : '', {
+				files: [ new Discord.Attachment(gif, 'spoiler.gif') ]
+			});
 		});
 	});
 };
