95c9e3102d3ff435e83c78e8a344b7845cb837e8
BaliBalo <balibalo@pause-geek.fr>
2018-01-18 03:28:34 +0000

    Add wave transition

--- scripts/spoiler/transitions.js
+++ scripts/spoiler/transitions.js
@@ -1,3 +1,5 @@
+// Testing : https://jsfiddle.net/o0marpyv/
+
 function genericRenderer(draw, n) {
 	return function(dest, from, to, onFrame) {
 		for (let i = 0; i < n; i++) {
@@ -55,7 +57,37 @@ let bandsAlternate = (function() {
 	return genericRenderer(frame, duration);
 })();
 
+let wave = (function() {
+	let bandSize = 20;
+	let duration = 50;
+	let bandDuration = .35;
+
+	function frame(i, ctx, from, to) {
+		let p = i / (duration - 1);
+		let w = ctx.canvas.width;
+		let h = ctx.canvas.height;
+		ctx.fillStyle = '#202731';
+		ctx.fillRect(0, 0, w, h);
+		for (let l = 0; l < w; l += bandSize) {
+			let bandFrom = (1 - bandDuration) * l / w;
+			let bandProgress = Math.min(Math.max((p - bandFrom) / bandDuration, 0), 1);
+			let bandZoom = 1 / (1 + Math.sin(bandProgress * Math.PI) * 1.333);
+			let im = bandProgress < .5 ? from : to;
+			let iw = bandSize;
+			let fw = iw * bandZoom;
+			let fh = h * bandZoom;
+			ctx.drawImage(from, l, 0, iw, h, l + (iw - fw) * .5, (h - fh) * .5, fw, fh);
+			ctx.globalAlpha = Math.max(Math.min((bandProgress - .4) / .2, 1), 0);
+			ctx.drawImage(to, l, 0, iw, h, l + (iw - fw) * .5, (h - fh) * .5, fw, fh);
+			ctx.globalAlpha = 1;
+		}
+	}
+
+	return genericRenderer(frame, duration);
+})();
+
 module.exports = {
 	bands,
-	bandsAlternate
+	bandsAlternate,
+	wave
 };
